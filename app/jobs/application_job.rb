640142a1805a0ec7ba75e326f6fbc44a4dffb594
moustafaMahran <moustafa.mahran208@gmail.com>
2022-10-04 04:53:56 +0200

    add morgue queue

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,5 +3,7 @@
 class ApplicationJob < ActiveJob::Base
   # in case of failure, retry job at most 2 times
   # in case of failure, retry job with wait interval minimum 5 minutes
-  retry_on(StandardError, wait: 5.minutes, attempts: 2)
+  retry_on(StandardError, wait: 5.minutes, attempts: 2) do |job|
+    job.enqueue(queue: "#{job.queue_name}-morgue")
+  end
 end
