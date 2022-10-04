92c91883f5760623a3932e1d446d6818d051176c
moustafaMahran <moustafa.mahran208@gmail.com>
2022-10-04 03:23:10 +0200

    adding application job

---
+++ app/jobs/application_job.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class ApplicationJob < ActiveJob::Base
+  # in case of failure, retry job at most 2 times
+  # in case of failure, retry job with wait interval minimum 5 minutes
+  retry_on(StandardError, wait: 5.minutes, attempts: 2)
+end
