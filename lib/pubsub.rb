75c3d51b297f5771f04fd7ea53a5d35ac6b3fe10
moustafaMahran <moustafa.mahran208@gmail.com>
2022-10-04 03:28:09 +0200

    adding pubsub class

--- lib/pubsub.rb
+++ lib/pubsub.rb
@@ -10,6 +10,34 @@ class Pubsub
   def topic(name)
     client.topic(name) || client.create_topic(name)
   end
+  # Find or create a subscription.
+  #
+  # @param subscription_id [String] The name of the topic to subscribe on
+  # @return [Google::Cloud::PubSub::Subscription]
+  def subscription(topic)
+    name = "worker-#{topic}"
+    client.subscription(name) || topic(topic).subscribe(name, enable_exactly_once_delivery: true,
+                                                              dead_letter_topic: dead_letter_topic,
+                                                              dead_letter_max_delivery_attempts: 2)
+  end
+
+  # create dead letter topic to keep track of dead jobs that failed after all retry attempts
+  def dead_letter_topic
+    topic("dead_letter_topic")
+  end
+
+  # to help checking if message should be processing now, or should be delayed
+  def fetch_message_delay(message)
+    timestamp = message_timestamp(message)
+    return 0 unless timestamp
+
+    (timestamp - Time.current.to_f).to_f
+  end
+
+  def retry_policy
+    retry_policy ||= Google::Cloud::PubSub::RetryPolicy.new(minimum_backoff: 300, maximum_backoff: 600)
+    retry_policy
+  end
 
   private
 
@@ -18,5 +46,13 @@ class Pubsub
   # @return [Google::Cloud::PubSub]
   def client
     @client ||= Google::Cloud::PubSub.new(project_id: "code-challenge")
+  end
+
+  # fetches message timestamp from attributes
+  def message_timestamp(message)
+    timestamp = message.attributes["timestamp"]
+    return nil unless timestamp
+
+    Time.at(timestamp.to_f)
   end
 end
