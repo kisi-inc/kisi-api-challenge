631fcb2090e76ffb62c8ef111acf0d833f6e3495
wcastillogc <97907331+wcastillogc@users.noreply.github.com>
2024-01-04 17:54:55 -0500

    added rescues for enqueues methods

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -2,6 +2,7 @@ class TestJob < ApplicationJob
   def perform(data)
     raise("Fake Failure") if rand < 0.2 # 20% chance of failure
 
+    # Job time duration
     sleep_duration = rand(5)
     sleep(sleep_duration)
 
--- lib/active_job/queue_adapters/pubsub_adapter.rb
+++ lib/active_job/queue_adapters/pubsub_adapter.rb
@@ -10,6 +10,10 @@ module ActiveJob
         serialized_job = serialize_job(job)
         pubsub = Pubsub.new
         pubsub.publish("general_jobs_queue", serialized_job)
+      rescue Google::Cloud::Error => e
+        Rails.logger.error("Unexpected GCP error while enqueueing job #{job.class.name}: #{e.message}")
+      rescue StandardError => e
+        Rails.logger.error("Unexpected error while enqueueing job #{job.class.name}: #{e.message}")
       end
 
       # Enqueue a job to be performed at a certain time.
@@ -21,6 +25,10 @@ module ActiveJob
         serialized_job = serialize_job(job, timestamp)
         pubsub = Pubsub.new
         pubsub.publish("general_jobs_queue", serialized_job)
+      rescue Google::Cloud::Error => e
+        Rails.logger.error("Unexpected GCP error while enqueueing job #{job.class.name}: #{e.message}")
+      rescue StandardError => e
+        Rails.logger.error("Unexpected error while enqueueing job #{job.class.name}: #{e.message}")
       end
 
       private
